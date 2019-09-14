86d7379cc365e8651079662b5ae0720edfb9fb52
Danny Ngo <dannyngo.1199@gmail.com>
2019-09-14 19:49:28 -0400

    this library is buggy

--- utils.py
+++ utils.py
@@ -11,38 +11,22 @@ def create_response_messages():
     return buttons
     
 def test_messages():
-    elements = []
-    
-    element = {
-        'title': 'Review',
-        'buttons': [
-            {
-                'type': 'web_url',
-                'title': '1',
-                'url': 'https://www.facebook.com'
-            },
-            {
-                'type': 'web_url',
-                'title': '2',
-                'url': 'https://www.facebook.com'
-            },
-            {
-                'type': 'web_url',
-                'title': '3',
-                'url': 'https://www.facebook.com'
-            },
-            {
-                'type': 'web_url',
-                'title': '4',
-                'url': 'https://www.facebook.com'
-            },
-            {
-                'type': 'web_url',
-                'title': '5',
-                'url': 'https://www.facebook.com'
-            }
-        ],
-        'image_url': 'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'
-    }
-    elements.append(element)
-    return elements
+    buttons = [
+        {
+            'type': 'web_url',
+            'title': 'test button1',
+            'url': 'https://www.facebook.com'
+        },
+        {
+            'type': 'web_url',
+            'title': 'test button2',
+            'url': 'https://www.facebook.com'
+        },
+        {
+            'type': 'web_url',
+            'title': 'test button3',
+            'url': 'https://www.facebook.com'
+        }
+    ]
+        
+    return buttons
