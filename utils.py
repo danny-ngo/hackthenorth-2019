f7d291795d098346ec9365e5516654d0d40aabc1
Danny Ngo <dannyngo.1199@gmail.com>
2019-09-14 19:22:43 -0400

    test generic message template

--- utils.py
+++ utils.py
@@ -8,3 +8,19 @@ def create_response_messages():
         buttons.append(button)
         
     return buttons
+    
+def test_messages():
+    elements = []
+
+    for i in range(1, 6):
+        element = {
+            'title': 'Review',
+            'buttons': [{
+                'type': 'web_url',
+                'title': str(i),
+                'url': 'https://www.facebook.com'
+            }],
+            'image_url': 'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'
+        }
+        elements.append(element)
+    return elements
