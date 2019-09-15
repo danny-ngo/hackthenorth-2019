d8811279793afa0e7796b2b87290b61d109f5235
Danny Ngo <dannyngo.1199@gmail.com>
2019-09-14 21:17:10 -0400

    testing quick replies

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os, sys
 from flask import Flask, request
 from pymessenger import Bot
-from utils import create_buttons
+import requests
+from utils import create_buttons, create_quick_replies
 
 app = Flask(__name__)
 
@@ -19,7 +20,48 @@ def verify():
         return request.args['hub.challenge'], 200
     return "Hello world", 200
     
+@app.route('/', methods=['POST'])
+def handle_messages():
+    data = request.get_json()
+    log(data)
     
+    for sender, message in messaging_events(data):
+        print 'Incoming from {}: {}'.format(sender, message)
+        send_message(PAGE_ACCESS_TOKEN, sender, message)
+    return "OK", 200
+
+def log(message):
+    print(message)
+    sys.stdout.flush()
+    
+def messaging_events(payload): 
+    '''
+    Generate tuples of (sender_id, message_text) from the provided payload
+    '''
+    data = json.loads(payload)
+    messaging_events = data['entry'][0]['messaging']
+    
+    for event in messaging_events:
+        if "message" in event and "text" in event['message']:
+            yield event['sender']['id'], event['message']['text'].encode('unicode_escape')
+        else:
+            yield event['sender']['id'], "Cannot echo this message"
+            
+def send_message(token, recipient, message):
+    '''
+    Send message to recipient id
+    '''
+    
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+            params={"access_token": token},
+            data=json.dumps({
+                "recipient": {"id": recipient},
+                "message": {"text": 'Hello',
+                            "quick_replies":create_quick_replies()}
+            }),
+            headers={'Content-type': 'application/json'})
+    
+'''    
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
@@ -48,7 +90,7 @@ def webhook():
 def log(message):
     print(message)
     sys.stdout.flush()
-
+'''
     
 if __name__ == "__main__":
     app.run(debug = True, port = 80)
